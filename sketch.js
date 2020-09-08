5e2f4477cb2339b7d4ad384530be2b561fddc0d6
Shramee Srivastav <shramee.srivastav@gmail.com>
2020-09-09 01:09:37 +0800

    Initial project commit

---
+++ sketch.js
@@ -0,0 +1,13 @@
+const
+  particles = [],
+  canvasWidth = window.innerWidth,
+  canvasHeight = window.innerHeight;
+
+function setup() {
+  createCanvas(canvasWidth, canvasHeight);
+  
+}
+
+function draw() {
+  background(220, 245, 255);
+}
